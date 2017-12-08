a5e21db267f629f60c933b030b309ce2f729f6ea
Joseph Maloney <j.maloney.ii@gmail.com>
2017-12-08 10:02:24 -0800

    add posts resources route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   #static pages
   get 'contact', to: 'static_pages#contact'
 
+  #blog routes
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
